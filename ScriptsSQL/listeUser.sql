8622195d46a558ddd1c1235620f37df52e450157
iOuls <remi.ls@live.fr>
2023-02-14 13:29:57 +0100

    Création des scripts SQL

---
+++ ScriptsSQL/listeUser.sql
@@ -0,0 +1,9 @@
+INSERT INTO user (email, roles, password, nom, prenom, telephone, administrateur, actif)
+VALUES
+    ('admin@admin.fr','["ROLE_ADMIN"]','$2y$13$28hfKYDt75uqyP9/IFWzy.3z5DJNZl93eOPxUJTgyfrnfQyvijxbm','admin','admin','0101010101',true,true),
+    ('thomas.lores2022@campus-eni.fr','[]','$2y$13$28hfKYDt75uqyP9/IFWzy.3z5DJNZl93eOPxUJTgyfrnfQyvijxbm','Lores','Thomas','0101010101',false,true),
+    ('thibaut.gerdelat2022@campus-eni.fr','[]','$2y$13$28hfKYDt75uqyP9/IFWzy.3z5DJNZl93eOPxUJTgyfrnfQyvijxbm','Gerdelat','Thibaut','0101010101',false,true),
+    ('remi.louis2022@campus-eni.fr','[]','$2y$13$28hfKYDt75uqyP9/IFWzy.3z5DJNZl93eOPxUJTgyfrnfQyvijxbm','Louis','Rémi','0101010101',false,true),
+    ('nicolas.richard2022@campus-eni.fr','[]','$2y$13$28hfKYDt75uqyP9/IFWzy.3z5DJNZl93eOPxUJTgyfrnfQyvijxbm','Richard','Nicolas','0101010101',false,true),
+    ('xavier.adenis2022@campus-eni.fr','[]','$2y$13$28hfKYDt75uqyP9/IFWzy.3z5DJNZl93eOPxUJTgyfrnfQyvijxbm','Adenis','Xavier','0101010101',false,true),
+    ('mickael.abin2022@campus-eni.fr','[]','$2y$13$28hfKYDt75uqyP9/IFWzy.3z5DJNZl93eOPxUJTgyfrnfQyvijxbm','Abin','Mickael','0101010101',false,true)
