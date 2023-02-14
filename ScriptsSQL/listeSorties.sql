341d7e60ca51dcc17a2c73ab052ac32298176321
iOuls <remi.ls@live.fr>
2023-02-14 14:16:43 +0100

    Modif base.html.twig

---
+++ ScriptsSQL/listeSorties.sql
@@ -0,0 +1,11 @@
+INSERT INTO sortie (etat_id,
+                    lieu_id,
+                    site_id,
+                    organisateur_id,
+                    nom,
+                    date_heure_debut,
+                    duree,
+                    date_limit_inscription,
+                    nb_inscriptions_max,
+                    infos_sortie)
+VALUES (1, 1, 1, 2, 'Sortie de Thomas',
